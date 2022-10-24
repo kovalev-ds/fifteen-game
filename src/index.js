603ef869c200af4a5aa86b7689e695e76aa53c09
kovvdn <kovvdn@gmail.com>
2022-10-24 16:54:34 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "minireset.css";
 import "./styles/main.scss";
 
-// import { createGame } from "./js/game";
-import { createElement } from "./js/lib";
+import { createElement, secToStringTime } from "./js/lib";
 
 import {
   createGame,
@@ -11,7 +10,6 @@ import {
   UP,
   DOWN,
   MOVE_EVENT,
-  MOVE_COUNTER_EVENT,
   START_EVENT,
   PLAY_PAUSE_EVENT,
   SOLVED_EVENT,
@@ -22,37 +20,48 @@ const MATRIX_SIZE = 4;
 
 const G_A_M_E = createGame(MATRIX_SIZE);
 
-G_A_M_E.subscribe(MOVE_EVENT, (idx, [x, y], { dx, dy }) => {
-  tiles[idx]
-    .animate(
-      [
-        {
-          transform: `translate(${-1 * dy * 100}%, ${-1 * dx * 100}%)`,
-        },
-      ],
-      150
-    )
-    .addEventListener("finish", () => {
-      tiles[idx].style.gridArea = `${x + 1} / ${y + 1}`;
-    });
 
-  G_A_M_E.isSolved &&
-    tiles.forEach((el) => {
-      el.animate(
-        [
-          {
-            transform: `rotate(360deg)`,
-          },
-        ],
-        250
-      );
-    });
+G_A_M_E.subscribe(MOVE_EVENT, (idx, [x, y], { dx, dy }) => {
+  const tile = tiles[idx]
+
+  const tileAnimation = tile.animate(
+    [
+      {
+        transform: `translate(${-1 * dy * 105}%, ${-1 * dx * 105}%)`,
+      },
+    ],
+    150
+  )
+
+  const handleFinishAnimation = () => {
+    tiles[idx].style.gridArea = `${x + 1} / ${y + 1}`;
+
+    G_A_M_E.isSolved &&
+      tiles.forEach((el) => {
+        el.animate(
+          [
+            {
+              transform: `rotate(360deg)`,
+            },
+          ],
+          250
+        );
+      });
+
+    tileAnimation.removeEventListener("finish", handleFinishAnimation)
+  }
+
+  tileAnimation.addEventListener("finish", handleFinishAnimation);
 
   movesControl.textContent = String(G_A_M_E.moves);
 });
 
-G_A_M_E.subscribe(START_EVENT, (matrix) => {
-  matrix.forEach((row, x) =>
+G_A_M_E.subscribe(START_EVENT, () => {
+
+  movesControl.textContent = String(G_A_M_E.moves);
+  timeControl.textContent = secToStringTime(G_A_M_E.duration);
+
+  G_A_M_E.matrix.forEach((row, x) =>
     row.forEach(({ idx }, y) => {
       tiles[idx].style.gridArea = `${x + 1} / ${y + 1}`;
 
@@ -68,32 +77,42 @@ G_A_M_E.subscribe(START_EVENT, (matrix) => {
   );
 });
 
-G_A_M_E.subscribe(PLAY_PAUSE_EVENT, (isPlaying) => {
-  let duration = 0;
-
-  setInterval(() => {
-    if (isPlaying) {
-      duration++;
-      timeControl.textContent = `${String((duration / 60) | 0).padStart(
-        2,
-        "0"
-      )}:${String(duration % 60).padStart(2, "0")}`;
-    }
-  }, 1000);
+G_A_M_E.subscribe(PLAY_PAUSE_EVENT, () => {
+  G_A_M_E.isPlaying ? overlay.classList.add('hidden') : overlay.classList.remove('hidden')
 });
+
+
+G_A_M_E.subscribe(TIMECHANGE_EVENT, () => {
+  timeControl.textContent = secToStringTime(G_A_M_E.duration);
+});
+
 
 G_A_M_E.subscribe(SOLVED_EVENT, () => {
-  alert("You WON");
-});
-
-G_A_M_E.subscribe(TIMECHANGE_EVENT, (duration) => {
-  timeControl.textContent = `${String((duration / 60) | 0).padStart(
-    2,
-    "0"
-  )}:${String(duration % 60).padStart(2, "0")}`;
+  console.log(`You WON!, It takes you ${G_A_M_E.moves} moves and ${G_A_M_E.duration} seconds`);
+  overlay.classList.remove("hidden")
 });
 
 // ==================================
+
+const title = createElement("h1", {
+  className: "app-title",
+  text: "Fifteen puzzle",
+});
+
+const movesControl = createElement("span", { text: "0" });
+const timeControl = createElement("span", { text: "00:00" });
+
+const startButton = createElement("button", {
+  className: "button",
+  text: "start new game",
+  events: {
+    click: () => {
+      G_A_M_E.start();
+      board.focus();
+    },
+  },
+})
+
 
 const tiles = G_A_M_E.matrix
   .map((row, x) =>
@@ -111,9 +130,20 @@ const tiles = G_A_M_E.matrix
   )
   .flat();
 
+const overlay = createElement('div', {
+  className: 'board-overlay', children: [
+    createElement("div", {
+      className: "stack stack--row stack--gap-x-2",
+      children: [
+        startButton
+      ],
+    }),
+  ]
+})
+
 const board = createElement("div", {
   className: `board board--size-${MATRIX_SIZE}`,
-  children: tiles,
+  children: [...tiles, overlay],
   tabindex: "0",
   events: {
     click: (e) => {
@@ -134,6 +164,7 @@ const board = createElement("div", {
       }
     },
     keydown: (e) => {
+
       switch (e.code) {
         case "ArrowUp":
           G_A_M_E.move(UP);
@@ -147,18 +178,13 @@ const board = createElement("div", {
         case "ArrowLeft":
           G_A_M_E.move(LEFT);
           break;
+        case "Escape": {
+          G_A_M_E.playpause();
+        }
       }
     },
   },
 });
-
-const title = createElement("h1", {
-  className: "app-title",
-  text: "Fifteen puzzle",
-});
-
-const movesControl = createElement("span", { text: "0" });
-const timeControl = createElement("span", { text: "00:00" });
 
 const controls = createElement("div", {
   className: "stack stack--align-center stack--gap-y-2",
@@ -176,31 +202,6 @@ const controls = createElement("div", {
         }),
       ],
     }),
-    createElement("div", {
-      className: "stack stack--row stack--gap-x-2",
-      children: [
-        createElement("button", {
-          className: "button",
-          text: "start game",
-          events: {
-            click: () => {
-              G_A_M_E.start();
-              board.focus();
-            },
-          },
-        }),
-        createElement("button", {
-          className: "button",
-          text: G_A_M_E.isPlaying ? "pause" : "resume",
-          events: {
-            click: () => {
-              G_A_M_E.playpause();
-            },
-          },
-        }),
-        createElement("button", { className: "button", text: "save game" }),
-      ],
-    }),
   ],
 });
 
@@ -210,111 +211,3 @@ const app = createElement("div", {
 });
 
 document.body.append(app);
-
-/* 
-
-const MATRIX_SIZE = 4;
-
-const G_A_M_E = createGame(MATRIX_SIZE, (state) => {
-  console.log(state);
-});
-
-const tiles = G_A_M_E.matrix
-  .map((row, x) =>
-    row.map(({ idx, name }, y) => {
-      const el = createElement("div", {
-        className: `tile ${G_A_M_E.empty.idx === idx ? "tile--empty" : ""}`,
-        text: name,
-        "data-idx": String(idx),
-      });
-
-      el.style.gridArea = `${x + 1} / ${y + 1}`;
-
-      return el;
-    })
-  )
-  .flat();
-
-const board = createElement("div", {
-  className: `board board--size-${G_A_M_E.matrix.length}`,
-  children: tiles,
-  events: {
-    click: (e) => {
-      const el = tiles.find((el) => el.contains(e.target));
-
-      if (el) {
-        const idx = Number(el.dataset.idx);
-        G_A_M_E.move(idx, ([x, y], { dx, dy }) => {
-          tiles[idx]
-            .animate(
-              [
-                {
-                  transform: `translate(${dy * 100}%, ${dx * 100}%)`,
-                },
-              ],
-              150
-            )
-            .addEventListener("finish", () => {
-              tiles[idx].style.gridArea = `${x + 1} / ${y + 1}`;
-            });
-        });
-      }
-    },
-  },
-});
-
-const title = createElement("h1", {
-  className: "app-title",
-  text: "Fifteen puzzle",
-});
-
-const controls = createElement("div", {
-  className: "stack stack--align-center stack--gap-y-2",
-  children: [
-    createElement("div", {
-      className: "stack stack--row stack--gap-x-2",
-      children: [
-        createElement("div", {
-          className: "info-box",
-          children: [
-            createElement("span", { text: "moves" }),
-            createElement("span", { text: "0" }),
-          ],
-        }),
-        createElement("div", {
-          className: "info-box",
-          children: [
-            createElement("span", { text: "time" }),
-            createElement("span", { text: "00:00" }),
-          ],
-        }),
-      ],
-    }),
-    createElement("div", {
-      className: "stack stack--row stack--gap-x-2",
-      children: [
-        createElement("button", {
-          className: "button",
-          text: "start game",
-          events: {
-            click: () => {
-              G_A_M_E.start((idx, [x, y]) => {
-                tiles[idx].style.gridArea = `${x + 1} / ${y + 1}`;
-              });
-            },
-          },
-        }),
-        createElement("button", { className: "button", text: "pause" }),
-        createElement("button", { className: "button", text: "save game" }),
-      ],
-    }),
-  ],
-});
-
-const app = createElement("div", {
-  className: "app",
-  children: [title, controls, board],
-});
-
-document.body.append(app);
-*/
